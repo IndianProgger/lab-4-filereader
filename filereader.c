ce9e9b018512224c5bd47288807959e80e5e4480
Fyodor Perevozchikov <31614726+IndianProgger@users.noreply.github.com>
2018-01-12 11:47:19 +0300

    Update filereader.c

--- filereader.c
+++ filereader.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
   FILE *file = NULL;
 
-  int i,c;
+  int i,c,s;
 
   int   num;
   char  string[256];
@@ -30,15 +30,18 @@ int main(int argc, char *argv[])
     printf("Error: this file can not be read!\n");
     exit(1);
   }
-  i = 1;
+  i = 0;
   while ((c = getc(file))!=EOF)
   {
     ungetc(c, file);
     fread(&num, sizeof(int), 1, file);
     fread(string, sizeof(char), 256, file);
     fread(&fl, sizeof(float), 1, file);
-    printf("Chunk %d:"   "\t%d"   "\t%s"   "\t%f\n", i, num, string, fl);
     i++;
+    printf("Chunk %d:"   "\t%d"   "\t%s"   "\t%f\n", i, num, string, fl);
   }
+  s=sizeof(num)+sizeof(string)+sizeof(fl);
+  s=s*i;
+  printf("Size: %d bytes",s);
   return 0;
 }
